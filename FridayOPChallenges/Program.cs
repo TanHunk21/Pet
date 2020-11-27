53ee7fa1644313e4f403c36296a62aa2eab4e39b
Opilane TTHK <opilane@tthk.ee>
2020-11-27 10:11:28 +0200

    first commit

---
+++ FridayOPChallenges/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FridayOPChallenges
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            } 
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine("I sleep like a boss!");
+                hunger += 0.2;
+            }
+
+            public void Meow()
+            {
+                Console.WriteLine("Meow meow im a king");
+            }
+
+            public void ShowCatDetails()
+            {
+                Console.WriteLine($"Name: {name};\n Color: {color}; \n Hunger: {hunger}; ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Cat newCat = new Cat("Juku", "Brown");
+
+            newCat.ShowCatDetails();
+
+            newCat.Meow();
+            
+
+        }
+    }
+}
